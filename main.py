db5633c21bf54b2d62636e61b5a39fff23921feb
Stanley Pan <imoment.developer@gmail.com>
2025-06-22 02:49:23 +0900

    add requirements.txt and main.py template

---
+++ main.py
@@ -0,0 +1,12 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+# ensures main() function is only called when file run directly
+# won't run if it's imported as a module
+if __name__ == "__main__":
+    main()
