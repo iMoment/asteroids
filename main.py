e802c287a4a68a30688a2eaadf8d953d21d6ed68
Stanley Pan <imoment.developer@gmail.com>
2025-06-22 05:37:00 +0900

    add collision logic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -18,12 +19,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    Player.containers = (updatable, drawable)
-    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     astroid_field = AsteroidField()
 
+    Player.containers = (updatable, drawable)
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
     while True:
@@ -32,6 +35,10 @@ def main():
                return
 
        updatable.update(dt) 
+       for asteroid in asteroids:
+           if asteroid.isCollidingWith(player_1):
+               sys.exit("Game over!")
+
        screen.fill("black")
        
        for item in drawable:
